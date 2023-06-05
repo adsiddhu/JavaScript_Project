441813b1a62d0ea7dd88cb4b17a2e2127dbc48b8
Adarsh <adsiddhu69@gmail.com>
2023-06-05 23:20:36 +0530

    Calculator

---
+++ Calculator/script.js
@@ -0,0 +1,54 @@
+let string = ''
+let buttons = document.querySelectorAll('.button')
+let multiply = document.querySelector('.multiply')
+let toggle_dark = document.querySelector('.btn-dark')
+let toggle_light = document.querySelector('.btn-light')
+let calculator = document.querySelector('.digital-calculator')
+let display = document.querySelector('.display')
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (Event) => {
+        if (Event.target.innerHTML == '=') {
+            string = eval(string)
+            document.querySelector('input').value = string
+        }
+
+        else if (Event.target.innerHTML == 'c') {
+            string = ''
+            document.querySelector('input').value = string
+        }
+
+        else if (Event.target.innerHTML == '%') {
+            string = eval(string) / 100
+            document.querySelector('input').value = string
+        }
+
+        else if (Event.target.innerHTML == '+/-') {
+            string = document.querySelector('input').value
+            document.querySelector('input').value = -1 * string
+        }
+
+        else {
+            console.log(Event.target);
+            string = string + Event.target.innerHTML
+            document.querySelector('input').value = string
+        }
+    })
+})
+
+toggle_dark.addEventListener('click', (Event) => {
+    console.log('Dark is Clicked');
+    // toggle_dark.innerHTML = 'Dark'
+    display.style.backgroundColor = 'rgba(32, 36, 45, 1)'
+    display.style.color = 'white'
+    calculator.style.backgroundColor = 'rgba(32, 36, 45, 1)'
+})
+
+toggle_light.addEventListener('click', (Event) => {
+    console.log('Light is Clicked');
+    // toggle_light.innerHTML = 'Dark'
+    display.style.backgroundColor = '#D0D3E2'
+    display.style.color = 'black'
+    calculator.style.backgroundColor = '#D0D3E2'
+})
+
